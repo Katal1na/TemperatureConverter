0250b042611ce5071656fb4777ef74c975d3c055
Katal1na <katia.yakushova@gmail.com>
2026-01-30 17:45:11 +0200

    Initial commit: add basic project info

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Temperature Converter App");
+        System.out.println("Celsius ↔ Fahrenheit");
+    }
+}
+
+
